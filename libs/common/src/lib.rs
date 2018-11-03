ae84e8a2424c72d25c4ac8a4b814b1420d97e8f6
Ryan1729 <Ryan1729@gmail.com>
2018-11-03 12:08:39 -0600

    pass down the features to only common

--- libs/common/src/lib.rs
+++ libs/common/src/lib.rs
@@ -1,15 +1,45 @@
+#[cfg(feature = "invariant-checking")]
+#[macro_export]
+macro_rules! invariant_check {
+    () => {
+        println!("invariant-checking {}:{}", file!(), line!());
+    };
+}
+
+#[cfg(not(feature = "invariant-checking"))]
+#[macro_export]
+macro_rules! invariant_check {
+    () => {};
+}
+
+#[cfg(feature = "logging")]
+#[macro_export]
+macro_rules! logging_check {
+    () => {
+        println!("logging {}:{}", file!(), line!());
+    };
+}
+
+#[cfg(not(feature = "logging"))]
+#[macro_export]
+macro_rules! logging_check {
+    () => {};
+}
+
+//In practice we would use these macros separately.
+//This part is just so we don't change the existing code.
 #[macro_export]
 macro_rules! check {
-    () => {{
-        if cfg!(feature = "invariant-checking") {
-            println!("invariant-checking {}:{}", file!(), line!());
-        }
-        if cfg!(feature = "logging") {
-            println!("logging {}:{}", file!(), line!());
-        }
+    () => {
+        common::invariant_check!();
+        common::logging_check!();
+    };
+    (common) => {{
+        invariant_check!();
+        logging_check!();
     }};
 }
 
 pub fn run() {
-    check!()
+    check!(common)
 }
